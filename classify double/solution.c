58b36c6539d84a291f2863774946fc5b3ecf8705
GreasyGoose <61307307+GreasyGoose@users.noreply.github.com>
2020-09-22 21:10:49 +0300

    First_homework

--- classify double/solution.c
+++ classify double/solution.c
@@ -14,8 +14,12 @@ uint64_t convertToUint64 (double number) {
     return *((uint64_t *)(&number));
 }
 
-bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+uint64_t getBit (const uint64_t number, const uint8_t index) { // changed bool to uint64 to simplify my solution
+    uint64_t mask = 1;
+    mask <<= index;
+    uint64_t temp = mask & number;
+    temp >>= index;
+    return temp;
 }
 
 
@@ -23,8 +27,36 @@ bool getBit (const uint64_t number, const uint8_t index) {
  * Checkers here:
  */
 
+
+/*Returns true for negative numbers*/
+bool isNegative (uint64_t number) {   
+    return ( getBit(number, 63) == 1 ); //check if bit #63 is equal to 1
+}
+
+bool isExpBitEqualTo (uint64_t number, uint64_t bit_value) { 
+    for (uint8_t i = 62; i > 51; i--){
+        if ( getBit(number, i) != bit_value ){
+            return 0;        
+        }
+    }
+    return 1;
+}
+
+
+// Exp bits == 0, last fraction bit == 1
+bool isDenormal (uint64_t number) {   
+    return ( isExpBitEqualTo(number, 0) ) && ( getBit(number, 0) == 1);
+}      
+
+
+// Exp bits are not simultaneously equal to 1 or to 0
+bool isNormal(uint64_t number) {   
+    return ( ! isExpBitEqualTo(number, getBit(number, 62)) );
+}  
+
+
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == 0x0000000000000000;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -32,37 +64,38 @@ bool checkForMinusZero (uint64_t number) {
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0x7ff0000000000000;
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0xfff0000000000000;
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    return ( ( ! isNegative(number) ) && isNormal(number) );
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    return ( isNegative(number)  && isNormal(number) );
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+    return ( (! isNegative(number)) && isDenormal(number)  );
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+    return ( isNegative(number)  && isDenormal(number) );
 }
 
+// Negative, exp bits ==1, first fraction bit ==0, last fraction bit ==1
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    return isExpBitEqualTo(number, 1) && getBit(number, 51) == 0 && getBit(number, 0) == 1;
 }
 
+// Negative, exp bits ==1, first fraction bit ==1
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    return isExpBitEqualTo(number, 1) && getBit(number, 51) == 1;
 }
-
 
 void classify (double number) {
     if (checkForPlusZero(convertToUint64(number))) {
