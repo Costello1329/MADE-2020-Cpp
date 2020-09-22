fbb3e8eef8a3db0cc5c69a428911d3e5d9ee326d
Lebedev <sergey.lebedev@corp.mail.ru>
2020-09-22 20:32:12 +0300

    homework done

--- classify double/solution.c
+++ classify double/solution.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -15,7 +16,47 @@ uint64_t convertToUint64 (double number) {
 }
 
 bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+    return number & (1 << index); 
+}
+
+bool hasZeroExponent (uint64_t number) {
+    for (int i=52; i<=62; i++) {
+        if (getBit(number, i)) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+bool isPositive (uint64_t number) {
+    return getBit(number, 63);
+}
+
+bool isNaN (uint64_t number) {
+    for (int i=52; i<=62; i++) {
+        if (!getBit(number, i)) {
+            return 0;
+        }
+    }
+
+    int all_bits_zero = 1;
+    for (int i=0; i<=51; i++) {
+        if (getBit(number, i)) {
+            all_bits_zero = 0;
+            break;
+        }
+    }
+
+    if (all_bits_zero) {
+        return 0;
+    }
+
+    return 1;
+}
+
+bool isDenormalized (uint64_t number) {
+    return hasZeroExponent(number) ? 1 : 0;
 }
 
 
@@ -24,7 +65,7 @@ bool getBit (const uint64_t number, const uint8_t index) {
  */
 
 bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+    return number == 0x0;
 }
 
 bool checkForMinusZero (uint64_t number) {
@@ -32,35 +73,43 @@ bool checkForMinusZero (uint64_t number) {
 }
 
 bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0x7FF0000000000000;
 }
 
 bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+    return number == 0xFFF0000000000000;
 }
 
 bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+    return !isDenormalized(number) && isPositive(number);
 }
 
 bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+    return !isDenormalized(number) && !isPositive(number);
 }
 
 bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+    return isDenormalized(number) && isPositive(number); 
 }
 
 bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+    return isDenormalized(number) && !isPositive(number);
 }
 
 bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+    if (!isNaN(number)) {
+        return 0;
+    }
+
+    return !getBit(number, 51);
 }
 
 bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+    if (!isNaN(number)) {
+        return 0;
+    }
+    
+    return getBit(number, 51);
 }
 
 
