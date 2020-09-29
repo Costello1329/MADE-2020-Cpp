3ac2f7aa42fcda64bef81912b34eb0c05a6b40bd
lebedevsergey <diacomltd@mail.ru>
2020-09-29 19:22:26 +0300

    save changed registers beyond r4 register

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -3,6 +3,9 @@
 count_det:
 .fnstart
 
+push {r5}
+push {r6}
+
 mov r1, $0
 
 ldr r2, [r0, $16]
@@ -10,37 +13,41 @@ ldr r3, [r0, $32]
 mul r6, r2, r3
 ldr r4, [r0, $20]
 ldr r5, [r0, $28]
-mul r7, r4, r5
-sub r2, r6, r7
+mul r3, r4, r5
+sub r2, r6, r3
 
-ldr r8, [r0]
-mul r8, r2, r8
-add r1, r1, r8
+ldr r3, [r0]
+mul r3, r2, r3
+add r1, r1, r3
 
 ldr r2, [r0, $12]
 ldr r3, [r0, $32]
 mul r6, r2, r3
 ldr r4, [r0, $24]
 ldr r5, [r0, $20]
-mul r7, r4, r5
-sub r2, r6, r7
+mul r3, r4, r5
+sub r2, r6, r3
 
-ldr r8, [r0, $4]
-mul r8, r2, r8
-sub r1, r1, r8
+ldr r3, [r0, $4]
+mul r3, r2, r3
+sub r1, r1, r3
 
 ldr r2, [r0, $12]
 ldr r3, [r0, $28]
 mul r6, r2, r3
 ldr r4, [r0, $24]
 ldr r5, [r0, $16]
-mul r7, r4, r5
-sub r2, r6, r7
+mul r3, r4, r5
+sub r2, r6, r3
 
-ldr r8, [r0, $8]
-mul r8, r2, r8
-add r1, r1, r8
+ldr r3, [r0, $8]
+mul r3, r2, r3
+add r1, r1, r3
 
 mov r0, r1
+
+pop {r6}
+pop {r5}
+
 bx lr
 .fnend
